a45354e0b939edf6a25b8d351c2420532188d0b4
Mateus <57414186+mcalves-stack@users.noreply.github.com>
2022-12-11 19:19:25 -0300

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
 
-DATABASE_URL = "postgresql://postgres:246581@localhost:5432/Labsoft"
+DATABASE_URL = "postgresql://postgres:246581@172.17.32.1:5432/Labsoft"
 
 
 engine = create_engine(DATABASE_URL)
