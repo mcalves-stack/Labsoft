8a9e0684d0c26ef7bbac2cf7214a979e0eff699e
Mateus <teus.alves.sp@gmail.com>
2022-12-13 11:10:41 -0300

    Main

--- main.py
+++ main.py
@@ -27,14 +27,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
 
 @app.post("/createuser", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
   hashed_password = utils.hash(user.password)
   user.password = hashed_password
 
-  print(user_id)
   new_user = models.Usuario(**user.dict())
   db.add(new_user)
   db.commit()
@@ -69,13 +72,13 @@ def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(o
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.get("/users")
-def get_users(db: Session = Depends(get_db),limit: int = 10, skip: int = 0, search: Optional[str] = ""):
+def get_users(db: Session = Depends(get_db),limit: int = 10, skip: int = 0, search: Optional[str] = "", user_id: int = Depends(oauth2.get_current_user)):
     users = db.query(models.Usuario).all()
     return {"data": users}
 
 
 @app.put("/users/{id}")
-def update_users(id: int, updated_post: schemas.UserCreate, db: Session = Depends(get_db)):
+def update_users(id: int, updated_post: schemas.UserCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
 
     users_query = db.query(models.Usuario).filter(models.Usuario.id == id)
 
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DATETIME
 from sqlalchemy.sql.expression import Null
 from database import Base
 
@@ -11,5 +11,26 @@ class Usuario(Base):
     materiaUsuario = Column(String, nullable=False)
     raUsuario = Column(Integer, nullable=False)
 
+class Sala(Base):
+    __tablename__ = "sala"
+
+    Id_sala = Column(Integer, primary_key=True, autoincrement=True)
+    ativa = Column(Boolean, nullable=False)
+    caixa_som = Column(Boolean, nullable=False)
+    capacidade = Column(Integer, nullable=False)
+    microfone = Column(Boolean, nullable=False)
+    nome_sala = Column(String, nullable=False)
+    projetor = Column(Boolean, nullable=False)
+    computador = Column(Integer, nullable=False)
+
+
+class Reserva(Base):
+    __tablename__ = "reserva"
+
+    IdReserva = Column(Integer, primary_key=True, autoincrement=True)
+    Data = Column(DATETIME, nullable=False)
+    IdSala = Column(Integer, nullable=False)
+    IdProfessor = Column(Integer, nullable=False)
+ 
 
     
